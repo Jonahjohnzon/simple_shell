53389ab71cee164ca1b93683eab4bbb5bb8aa828
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-20 10:23:53 -0700

    strcat

--- _strcat.c
+++ _strcat.c
@@ -1,14 +1,15 @@
-#include "main.h"
-void _strcat(char *des,const char *src) {
-    while (*des != '\0') {
-        des++;
-    }
-
-    while (*src != '\0') {
-        *des = *src;
-        des++;
-        src++;
-    }
-    *des = '\0';
+#inlcude "main.h"
+void _strcat(char *des,const char *src)
+{
+	while (*des != '\0')
+	{
+		des++;
+	}
+	while (*src != '\0')
+	{
+		*des = *src;
+		des++;
+		src++;
+	}
+	*des = '\0';
 }
-
