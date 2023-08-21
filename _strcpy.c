313e4a445af17d93b4d25e8491e8b4564badd1cb
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-21 07:32:19 -0700

    strcpy

---
+++ _strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - strcpy function
+ * Description: copy strings pointed to.
+ * @src: string
+ * @des: pointer destination
+ */
+
+void _strcpy(char *des, const char *src)
+{
+	while (*src != '\0')
+	{
+		*des = *src;
+		des++;
+		src++;
+	}
+
+	*des = '\0';
+
+	return (0);
+}
