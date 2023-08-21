159899edb667711441f2ea268e463d0e14a99ebc
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-21 03:48:24 -0700

    strtok

---
+++ strtok.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strtok - strtok function
+ * Description: Breaks strings into series of tokens
+ * @p: string
+ * @tokens: C string of the delimiters
+ */
+void _strtok(char *p, char **tokens)
+{
+	char *delim = " ";
+
+	int tokenCount = 0;
+	char *token = strtok(p, delim);
+
+	while (token != NULL && tokenCount < 200)
+	{
+		tokens[tokenCount] = token;
+		tokenCount++;
+		token = strtok(NULL, delim);
+	}
+	tokens[tokenCount] = NULL;
+
+	return (0);
+}
