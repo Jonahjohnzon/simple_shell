128acecf637e211f9988f8241993c4a20a264647
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-23 04:11:50 -0700

    project

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - calculate length of a string
+ * @str: string
+ *
+ * Return: i
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t i = 0;
+
+	while (str[l] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
