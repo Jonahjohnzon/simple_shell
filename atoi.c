4de02ef9d2cb4c4a942342aad498823e5c3689d3
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-23 10:40:48 -0700

    project

---
+++ atoi.c
@@ -0,0 +1,29 @@
+#include "shell.c"
+/**
+ * _atoi - converts a string into an integer
+ *@str: string represntation
+ *Return: int
+ */
+int *_atoi(char *str)
+{
+	int i, j, k;
+
+	k = 1;
+	i = 0;
+	j = 0;
+
+	while (!((str[i] >= '0') && (str[i] <= '9')) && (str[i] != '\0'))
+	{
+		if (str[i] == '-')
+		{
+			k = k * (-1);
+		}
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		j = (j * 10) + (k * (str[i] - '0'));
+		i++;
+	}
+	return (i);
+}
