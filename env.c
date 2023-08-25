521d6935ba5fda74c0d0d891909149c5c9ce5ef4
Taiwo Adeyemi <Tadeyemi004@outlook.com>
2023-08-25 04:34:02 -0700

    project

---
+++ env.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+/**
+ * _unsetenv - deletes the variable name from an environment
+ * @info: Structured arguments
+ * @var: the environment variable string
+ * Return: 1 on delete, 0 otherwise
+ */
+int _unsetenv(info_table *info, char *var)
+{
+	list_t *node = info->env;
+	size_t a = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), a);
+			a = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		a++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - add or modify the variable name with value to the environment
+ * @info: Structured arguments.
+ * @var: the environment variable string
+ * @value: the environment variable value
+ * Return: 0 on success
+ */
+int _setenv(info_table *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
